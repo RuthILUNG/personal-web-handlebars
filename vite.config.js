e884cd14946c2352b2e9d1c8f4b9549d8a1c869e
Bajana <rilunga323@gmail.com>
2024-06-05 22:58:54 +0200

    first commit

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import handlebars from 'vite-plugin-handlebars';
+import data from './data.json';
+export default {
+  plugins: [
+    handlebars({
+      context: data, 
+    }),
+  ],
+};
+
+const basicContext = {
+        title : "Hello this is some text",
+        show : false,
+        fruits : [
+            "apples",
+            "oranges",
+            "bananas"
+        ],
+        isClient : true,
+        fruitSales : [
+            {day : "Monday", apples : 13, oranges : 34},
+            {day : "Tuesday", apples : 17, oranges : 28},
+            {day : "Thursday", apples : 7, oranges : 45},
+        ]
+    };
+
